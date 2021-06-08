2a52f5839c76332fa76cacf01591c1bd88554f29
kilitary <kilitary@gmail.com>
2021-06-08 04:20:59 +0300

    ss

--- cnn.py
+++ cnn.py
@@ -78,44 +78,87 @@ if __name__ == '__main__':
 
 	HOST = 'edition.cnn.com'
 	PORT = 443
-	data = requests.get(f'https://{HOST}').text
-	# flog(f'{data}')
-	matches = re.findall(r"//(.*?\.[^ /\",:\?]*)", data, flags=re.RegexFlag.S | re.RegexFlag.M)
-	flog(f'ret: {matches}', fprint=True)
-	domains = {}
-	for match in matches:
-		if domains.get(match) is None:
-			domains[match] = 1
-		else:
-			domains[match] = domains[match] + 1
 
-	for domain in domains:
-		#	flog(f'{domain}: {domains[domain]}', fprint=True)
+	random.seed()
 
-		start = time.perf_counter_ns()
-		try:
-			data = requests.get(f'https://{domain}/')
-			content = data.text
-			duration = (time.perf_counter_ns() - start) / 1000000
+	while True:
+		data = requests.get(f'https://{HOST}').text
+		# flog(f'{data}')
+		matches = re.findall(r"//(.*?\.[^ /\",:\?]*)", data, flags=re.RegexFlag.S | re.RegexFlag.M)
+		flog(f'ret: {matches}', fprint=True)
+		domains = {}
+		for match in matches:
+			if domains.get(match) is None:
+				domains[match] = 1
+			else:
+				domains[match] += 1
 
-			# with connection:
-			cursor = connection.cursor()
-			# Create a new record
-			cookies = "\n".join(data.cookies)
-			headers = "\n".join(data.headers.values())
-			server = data.headers.get('server')
-			code = data.status_code
-			sql = f"INSERT INTO `domains` (`host`, `ping_ms`, `server`, `cookies`, `headers`, `content`, `code`)" \
-			      f"VALUES ('{domain}', {duration}, '{server}', '{cookies}', '{headers}', '{content}', {code})"
+		def take_random(elem):
+			return random.randint(0, 1)
 
-			print(f"[{code}] {domain}: len {len(content)}b, dur {duration}ms. ")
-			ret = cursor.execute(sql)
-			cursor.close()
+		domains = sorted(domains, key=take_random)
 
-			# connection is not autocommit by default. So you must commit to save
-			# your changes.
-			connection.commit()
+		for domain in domains:
+			#	flog(f'{domain}: {domains[domain]}', fprint=True)
+			domain = re.sub(r'([^.0-9a-zA-Z])', '', domain)
 
-		except Exception as e:
-			flog(f'exception: {e}')
-			continue
+			start = time.perf_counter_ns()
+			try:
+				data = requests.get(f'https://{domain}/', timeout=10)
+				content = data.text
+				duration = (time.perf_counter_ns() - start) / 1000000
+
+				# with connection:
+				cursor = connection.cursor()
+				# Create a new record
+				extensions = data.headers.get('x-powered-by')
+				cookies = "\n".join(data.cookies.values()) if data.cookies else ''
+				headers = "\n".join(data.headers.values())
+				server = data.headers.get('server')
+				code = data.status_code
+
+				cur = connection.cursor()
+				cur.execute(f"SELECT id FROM domains WHERE host='{domain}'")
+				id = cur.fetchone()
+				id = id['id'] if id else None
+
+				server = connection.escape_string(server) if server else ''
+				cookies = connection.escape_string(cookies) if cookies else ''
+				headers = connection.escape_string(headers) if headers else ''
+				content = connection.escape_string(content) if content else ''
+				#content = re.sub(r'([^a-z0-9 \[\]~!@#\$%\^&\*\(\)<>\'"\/]+)+', '', content)
+				extensions = connection.escape_string(extensions) if extensions else ''
+
+				if id is not None:
+					sql = f"UPDATE `domains` SET `host` = '{domain}', `ping_ms` = {duration}, `server` = '{server}'," \
+					      f"`cookies` = '{cookies}', `headers` = '{headers}', `content` = '{content}', `code` = {code}, `extensions` = '{extensions}'" \
+					      f" WHERE id = {id}"
+				else:
+					sql = f"INSERT INTO `domains` (`host`, `ping_ms`, `server`, `cookies`, `headers`, `content`, `code`, `extensions`)" \
+					      f"VALUES ('{domain}', {duration}, '{server}', '{cookies}', '{headers}', '{content}', {code}, '{extensions}')"
+
+				cursor.execute(sql)
+				cursor.close()
+
+				print(f"[{code}] {domain}: len {len(content)}b, dur {duration}ms. ")
+
+				# connection is not autocommit by default. So you must commit to save
+				# your changes.
+				connection.commit()
+
+			except Exception as e:
+				# traceback.print_exc()
+				type, value, traceback = sys.exc_info()
+				# print(f'-{value}|-{type}|-{traceback}|')
+				msg = re.sub(r'([\'"])', '', str(value))
+				print(f'[ ! ] {domain}: exception: {msg} [{sys.exc_info()[2]}]')
+				sql = f"INSERT IGNORE INTO `domains` SET `error` = '{msg}', host = '{domain}'"
+				cursor = connection.cursor()
+				cursor.execute(sql)
+				cursor.close()
+				connection.commit()
+				continue
+			time.sleep(random.randint(1, 5))
+
+		print(f'ok, wait ...')
+		time.sleep(5)
