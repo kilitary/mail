544177de248a9f441bd7043fc8f54aa2b18e1dc9
kilitary <kilitary@gmail.com>
2021-06-12 00:44:53 +0300

    ss

--- cnn.py
+++ cnn.py
@@ -31,6 +31,23 @@ from urllib import parse
 from torpy.http.requests import do_request as http
 import requests
 import pymysql.cursors
+from sqlescapy import sqlescape
+from nmi_mysql import nmi_mysql
+import MySQLdb
+from datetime import date, datetime, timedelta
+import mysql.connector
+import requests.utils
+import requests.cookies
+
+db_conf = {
+	'host'         : 'kilitary.ru',
+	'user'         : 'cnn',
+	'password'     : 'cnn',
+	'db'           : 'cnn',
+	'port'         : 3306,
+	'max_pool_size': 20  # optional, default is 10
+}
+log_file = 'cnn.log'
 
 class Randomer(object):
 	def __init__(self):
@@ -69,17 +86,27 @@ def flog(msg, fprint=False):
 
 if __name__ == '__main__':
 	print(f'connecting to db ...')
-	connection = pymysql.connect(host='kilitary.ru',
-	                             user='cnn',
-	                             password='cnn',
-	                             database='cnn',
-	                             cursorclass=pymysql.cursors.DictCursor)
-	print(f'done')
+	# connection = pymysql.connect(host='kilitary.ru',
+	#                              user='cnn',
+	#                              password='cnn',
+	#                              database='cnn',
+	#                              cursorclass=pymysql.cursors.DictCursor)
+	# db = nmi_mysql.DB(db_conf)
+	cnx = mysql.connector.connect(host='kilitary.ru', user='cnn', password='cnn', database='cnn', autocommit=True)  # , sql_mode="ANSI_QUOTES"
+	db = cnx.cursor(buffered=True, dictionary=True)
+	print(f'done {cnx.get_server_info()}')
+
+	# cursor.execute("insert into domains (host, error) values(%s, %d)", ['df', 304])
+	# cnx.commit()
+	# sys.exit(0)
 
 	HOST = 'edition.cnn.com'
 	PORT = 443
 
 	random.seed()
+
+	if os.path.exists(log_file):
+		os.unlink(log_file)
 
 	while True:
 		data = requests.get(f'https://{HOST}').text
@@ -99,64 +126,83 @@ if __name__ == '__main__':
 		domains = sorted(domains, key=take_random)
 
 		for domain in domains:
-			#	flog(f'{domain}: {domains[domain]}', fprint=True)
+			id = None
+			print(f'\rconnecting to {domain} ...', end='')
 			domain = re.sub(r'([^.0-9a-zA-Z])', '', domain)
+
+			if 'cnn' not in domain:
+				continue
 
 			start = time.perf_counter_ns()
 			try:
-				data = requests.get(f'https://{domain}/', timeout=10)
+				try:
+					data = requests.get(f'https://{domain}/', timeout=10)
+				except Exception as e:
+					print(f'exception HTTP={e}')
+					continue
+
 				content = data.text
 				duration = (time.perf_counter_ns() - start) / 1000000
 
 				# with connection:
-				cursor = connection.cursor()
+				# cursor = connection.cursor()
 				# Create a new record
 				extensions = data.headers.get('x-powered-by')
-				cookies = "\n".join(data.cookies.values()) if data.cookies else ''
+				cookies = data.cookies if data.cookies else ''
 				headers = "\n".join(data.headers.values())
 				server = data.headers.get('server')
 				code = data.status_code
 
-				cur = connection.cursor()
-				cur.execute(f"SELECT id FROM domains WHERE host='{domain}'")
-				id = cur.fetchone()
-				id = id['id'] if id else None
+				# cur = connection.cursor()
+				# cur.execute(f"SELECT id FROM domains WHERE host='{domain}'")
+				db.execute('SELECT id FROM domains WHERE host = %s', [domain])
+				exist = db.fetchone()
 
-				server = connection.escape_string(server) if server else ''
-				cookies = connection.escape_string(cookies) if cookies else ''
-				headers = connection.escape_string(headers) if headers else ''
-				content = connection.escape_string(content) if content else ''
-				#content = re.sub(r'([^a-z0-9 \[\]~!@#\$%\^&\*\(\)<>\'"\/]+)+', '', content)
-				extensions = connection.escape_string(extensions) if extensions else ''
+				id = exist.get('id') if exist else None
+				# print(f'exist: {id}')
+				server = sqlescape(server) if server else ''
+				cookies = ':'.join(cookies) if isinstance(cookies, requests.cookies.RequestsCookieJar) else cookies
+				headers = MySQLdb.escape_string(headers) if headers else ''
+				# content = connection.escape_string(content) if content else ''
+				# content = "%s" % sqlescape(content)
+				content = MySQLdb.escape_string(content)
+				# content = sqlescape(content)
+				# content = re.sub(r'([^a-z0-9 \[\]~!@#\$%\^&\*\(\)<>\'"\/]+)+', '', content)
+				extensions = MySQLdb.escape_string(extensions) if extensions else ''
 
-				if id is not None:
-					sql = f"UPDATE `domains` SET `host` = '{domain}', `ping_ms` = {duration}, `server` = '{server}'," \
-					      f"`cookies` = '{cookies}', `headers` = '{headers}', `content` = '{content}', `code` = {code}, `extensions` = '{extensions}'" \
-					      f" WHERE id = {id}"
-				else:
-					sql = f"INSERT INTO `domains` (`host`, `ping_ms`, `server`, `cookies`, `headers`, `content`, `code`, `extensions`)" \
-					      f"VALUES ('{domain}', {duration}, '{server}', '{cookies}', '{headers}', '{content}', {code}, '{extensions}')"
+				# print(f'domain: [{domain}, id={id}]')
 
-				cursor.execute(sql)
-				cursor.close()
+				# print(f'sql: [{sql}]')
+				sql = ''
+				try:
+					if id is not None:
+						db.execute("UPDATE domains SET host = %s, ping_ms = %s, server = %s, cookies = %s, headers = %s, content = %s, code = %s, extensions = %s WHERE id = %s",
+						           [domain, str(duration), server, cookies, headers, content, str(code), extensions, str(id)])
+					else:
+						db.execute("INSERT INTO domains (`host`, `ping_ms`, `server`, `cookies`, `headers`, `content`, `code`, `extensions`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+						           [domain, str(duration), server, cookies, headers, content, str(code), extensions])
+				except Exception as e:
+					print(f'\rexception sql: {e} [{db.statement}] {cnx.sql_mode}')
+					flog(f'exception {e} domain:{domain}={db.statement}')
+					quit()
 
-				print(f"[{code}] {domain}: len {len(content)}b, dur {duration}ms. ")
+				print(f"\r[{code}] [{domain}, id={id}]: len {len(content)}b, dur {duration}ms., redir {True if data.is_redirect else False} ")
 
 				# connection is not autocommit by default. So you must commit to save
 				# your changes.
-				connection.commit()
+				cnx.commit()
 
 			except Exception as e:
-				# traceback.print_exc()
+				traceback.print_exc()
 				type, value, traceback = sys.exc_info()
-				# print(f'-{value}|-{type}|-{traceback}|')
+				print(f'-{value}|-{type}|-{traceback}|')
 				msg = re.sub(r'([\'"])', '', str(value))
 				print(f'[ ! ] {domain}: exception: {msg} [{sys.exc_info()[2]}]')
-				sql = f"INSERT IGNORE INTO `domains` SET `error` = '{msg}', host = '{domain}'"
-				cursor = connection.cursor()
-				cursor.execute(sql)
-				cursor.close()
-				connection.commit()
+				db.execute(f"INSERT IGNORE INTO `domains` SET `error` = '{msg}', host = '{domain}'")
+				# cursor = connection.cursor()
+				# cursor.execute(sql)
+				# cursor.close()
+				# connection.commit()
 				continue
 			time.sleep(random.randint(1, 5))
 
