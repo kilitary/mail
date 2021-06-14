59579105f86477bc834b7e551013b536183f0eb3
kilitary <kilitary@gmail.com>
2021-06-15 01:07:16 +0300

    ss

--- kilitary-py-server/web-service.py
+++ kilitary-py-server/web-service.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 import asyncio
-# WS server example
 import json
 import websockets
 import logging
 
-async def onConnection(websocket, path):
-	print(f'connected {websocket.remote_address}')
+async def processConnection(websocket, path):
+	print(f'connected {websocket.remote_address}:{websocket.local_address}')
 	message = ''
 	try:
 		message = await websocket.recv()
@@ -16,8 +15,20 @@ async def onConnection(websocket, path):
 
 	print(f"> {json.loads(message)}")
 	print(f'process_message ... {path} {type(message)}')
-	message = json.dumps({'test1': 1, 'test2': 2})
+	message = json.dumps({'fail': False, 'data': 200})
 	print(f"< {message}")
+	try:
+		await websocket.send(message)
+	except Exception as e:
+		print(f'exception: {e}')
+
+	try:
+		message = await websocket.recv()
+	except Exception as e:
+		print(f'exception: {e}')
+	print(f"> {json.loads(message)}")
+
+	message = json.dumps({'error': False, 'id': 117})
 	try:
 		await websocket.send(message)
 	except Exception as e:
@@ -27,8 +38,8 @@ logger = logging.getLogger("websockets.server")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-start_server = websockets.serve(onConnection, "192.168.10.1", 18765)
-print(f'we run')
+start_server = websockets.serve(processConnection, "192.168.10.1", 18765)
+print(f'we run on 192.168.10.1:18765')
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
